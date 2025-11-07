1a8280d8526d297933a6ba4255aaa08f1e093da7
Wilson Franca <wilson.franca.92@gmail.com>
2025-11-06 21:52:46 -0800

    chore: initial setup with eslint, prettier, and lefthook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+
+import { dirname } from 'path';
+
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,15 +12,21 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'src/generated/prisma/**',
+      'pgdata/**',
+      '.next/**',
+      'node_modules/**',
     ],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+    },
   },
 ];
 
