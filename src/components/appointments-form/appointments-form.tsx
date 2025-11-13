2aa082df96a8415235c1a6fa82263299a3448edd
Wilson Franca <wilson.franca.92@gmail.com>
2025-11-12 17:14:36 -0800

    feat(appointments-form): add phone number input with mask

--- src/components/appointments-form/appointments-form.tsx
+++ src/components/appointments-form/appointments-form.tsx
@@ -20,8 +20,9 @@ import {
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Cat, User } from 'lucide-react';
+import { Cat, Phone, User } from 'lucide-react';
 import { useForm } from 'react-hook-form';
+import { IMaskInput } from 'react-imask';
 import z from 'zod';
 
 const appointmentsFormSchema = z.object({
@@ -82,6 +83,30 @@ export const AppointmentsForm = () => {
                       <Input
                         placeholder="Tutor name"
                         className="pl-10"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Phone number
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Phone className="size-5 absolute left-3 top-1/2 -translate-y-1/2 transform text-content-brand" />
+                      <IMaskInput
+                        placeholder="(999) 999-9999"
+                        mask="(000) 000-0000"
+                        className="pl-10 flex h-12 w-full rounded-md border border-border-primary bg-background-tertiary px-3 py-2 text-sm text-content-primary ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-content-secondary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-0 focus-visible:ring-border-brand disabled:cursor-not-allowed disabled:opacity-50 hover:border-border-secondary focus:border-border-brand focus-visible:border-border-brand aria-invalid:ring-destructive/20 aria-invalid:border-destructive"
                         {...field}
                       />
                     </div>
