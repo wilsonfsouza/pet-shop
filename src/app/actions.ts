a8ca2883d1fffb50191db6051cb1a1a6e365e105
Wilson Franca <wilson.franca.92@gmail.com>
2025-11-15 10:16:06 -0800

    styles/chore: add alert-dialog, calendar, popover, select from Shadcn UI and updated styles. Correct package install for toast component

---
+++ src/app/actions.ts
@@ -0,0 +1,150 @@
+'use server';
+
+import { prisma } from '@/lib/prisma';
+import { revalidatePath } from 'next/cache';
+import z from 'zod';
+
+const appointmentSchema = z.object({
+  tutorName: z.string(),
+  petName: z.string(),
+  phone: z.string(),
+  description: z.string(),
+  scheduledAt: z.date(),
+});
+
+type CreateAppointmentDTO = z.infer<typeof appointmentSchema>;
+
+export async function createAppointment(data: CreateAppointmentDTO) {
+  try {
+    const parsedData = appointmentSchema.parse(data);
+
+    const { scheduledAt } = parsedData;
+    const hour = scheduledAt.getHours();
+
+    const isMorning = hour >= 9 && hour < 12;
+    const isAfternoon = hour >= 13 && hour < 18;
+    const isEvening = hour >= 19 && hour < 21;
+
+    if (!isMorning && !isAfternoon && !isEvening) {
+      return {
+        error:
+          'Appointments can only be scheduled between 9am-12pm, 1pm-6pm, or 7pm-9pm',
+      };
+    }
+
+    const existingAppointment = await prisma.appointment.findFirst({
+      where: {
+        scheduledAt,
+      },
+    });
+
+    if (existingAppointment) {
+      return {
+        error: 'This time slot is already reserved',
+      };
+    }
+
+    const currentDate = new Date();
+
+    await prisma.appointment.create({
+      data: {
+        ...parsedData,
+        createdAt: currentDate,
+      },
+    });
+
+    revalidatePath('/');
+  } catch (error) {
+    console.log(error);
+    return {
+      error: 'Error while creating appointment. Try again later.',
+    };
+  }
+}
+
+type UpdateAppointmentDTO = z.infer<typeof appointmentSchema>;
+
+export async function updateAppointment(
+  id: string,
+  data: UpdateAppointmentDTO
+) {
+  try {
+    const parsedData = appointmentSchema.parse(data);
+
+    const { scheduledAt } = parsedData;
+    const hour = scheduledAt.getHours();
+
+    const isMorning = hour >= 9 && hour < 12;
+    const isAfternoon = hour >= 13 && hour < 18;
+    const isEvening = hour >= 19 && hour < 21;
+
+    if (!isMorning && !isAfternoon && !isEvening) {
+      return {
+        error:
+          'Appointments can only be scheduled between 9am-12pm, 1pm-6pm, or 7pm-9pm',
+      };
+    }
+
+    const existingAppointment = await prisma.appointment.findFirst({
+      where: {
+        scheduledAt,
+        id: {
+          not: id,
+        },
+      },
+    });
+
+    if (existingAppointment) {
+      return {
+        error: 'This time slot is already reserved',
+      };
+    }
+
+    await prisma.appointment.update({
+      where: {
+        id,
+      },
+      data: {
+        ...parsedData,
+      },
+    });
+
+    revalidatePath('/');
+  } catch (error) {
+    console.log(error);
+
+    return {
+      error: 'Error while updating appointment. Try again later.',
+    };
+  }
+}
+
+export async function removeAppointment(id: string) {
+  try {
+    const existingAppointment = await prisma.appointment.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!existingAppointment) {
+      return {
+        error: 'Selected appointment does not exist',
+      };
+    }
+
+    await prisma.appointment.delete({
+      where: {
+        id,
+      },
+    });
+
+    revalidatePath('/');
+  } catch (error) {
+    console.log(error);
+
+    return {
+      error: 'Error while removing appointment. Try again later.',
+    };
+  }
+}
