f1c9d0c3f16d39921ae567fad3932d9c47c7ea5e
Wilson Franca <wilson.franca.92@gmail.com>
2025-11-20 00:45:43 -0800

    chore: add prisma client to output file tracing includes at next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingIncludes: {
+    '/*': ['./node_modules/.prisma/client/**/*'],
+  },
 };
 
 export default nextConfig;
